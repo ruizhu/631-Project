bae291997f42125ce6d0e5bcecc436eaefe6d4a3
Peter Tan <tanhp23@tamu.edu>
2018-04-04 22:17:19 -0500

    use cookie to verify signin status

--- php/initialize.php
+++ php/initialize.php
@@ -2,4 +2,10 @@
   ini_set('display_errors', 1);
   require_once('database.php');
   $db = db_connect();
+
+  function check_signin() {
+    if($_COOKIE["PHPSESSID"] == NULL) {
+      header('Location: ../signin.php');
+    }
+  }
 ?>
