e8aa8caa30877416da875c5a7d82044a02b62624
Peter Tan <tanhp23@tamu.edu>
2018-04-20 17:18:24 -0500

    double checked

--- php/table_desktop.php
+++ php/table_desktop.php
@@ -4,35 +4,56 @@
   } else {
     echo "<table class=\"col-12 table table-striped table-hover rounded-bottom\" id=\"main_table\">";
     while($post = mysqli_fetch_assoc($posts)) {
-      echo "<tr data-toggle=\"modal\" data-target=\"#postDetailModel\" data-post_id=";
+      echo "<tr data-toggle=\"modal\" data-target=\"#postDetailModel\" id=\"post_id_";
       echo $post["post_id"];
-      echo ">";
-        echo "<td class=\"list_column_icon\" style=\"width: 7%\">";
-        if($post["image"] != ''){
-          echo "<i class=\"material-icons\" style=\"width: 7%\">camera_alt</i>";
-        }
-        echo "</td>";
+      echo "\"";
 
-        echo "<td class=\"list_column_title\" style=\"width: 50%\">";
-        echo $post["title"];
-        echo "</td>";
+      echo "<td class=\"d-none\">";
+      echo $post["post_id"];
+      echo "</td>";
+      echo "<td class=\"d-none\">";
+      echo $post["price"];
+      echo "</td>";
+      echo "<td class=\"d-none\">";
+      echo $post["contact"];
+      echo "</td>";
+      echo "<td class=\"d-none\">";
+      echo $post["description"];
+      echo "</td>";
+      echo "<td class=\"d-none\">";
+      echo $post["image"];
+      echo "</td>";
 
-        echo "<td class=\"list_column_icon\" style=\"width: 7%\">";
+
+      echo "<td class=\"list_column_icon\" style=\"width: 7%\">";
+      if($post["image"] != ''){
+        echo "<i class=\"material-icons\" style=\"width: 7%\">camera_alt</i>";
+      }
+      echo "</td>";
+
+      echo "<td class=\"list_column_title\" style=\"width: 50%\">";
+      echo $post["title"];
+      echo "</td>";
+
+      echo "<td class=\"list_column_icon\" style=\"width: 7%\">";
+      if (check_bookmarked($user_id, $post["post_id"])) {
         echo "<i class=\"material-icons\" style=\"width: 7%\">bookmark</i>";
-        echo "</td>";
+      }
+      echo "</td>";
 
-        echo "<td class=\"list_column_date\" style=\"width: 10%\">";
-        echo substr($post["post_date"], 5, 5);
-        echo "</td>";
+      echo "<td class=\"list_column_date\" style=\"width: 10%\">";
+      echo substr($post["post_date"], 5, 5);
+      echo "</td>";
 
-        if ($post["purpose"] != 'Looking'){
-          echo "<td class=\"list_column_icon\" style=\"width: 7%\"><i class=\"material-icons\">local_offer</i></td>";
-        } else {
-          echo "<td class=\"list_column_icon\" style=\"width: 7%\"><i class=\"material-icons\">search</i></td>";
-        }
-        echo "<td class=\"list_column_price\" style=\"width: 19%\">";
-        echo $post["price"];
-        echo "</td>";
+      if ($post["purpose"] != 'Looking'){
+        echo "<td class=\"list_column_icon\" style=\"width: 7%\"><i class=\"material-icons\">local_offer</i></td>";
+      } else {
+        echo "<td class=\"list_column_icon\" style=\"width: 7%\"><i class=\"material-icons\">search</i></td>";
+      }
+      echo "<td class=\"list_column_price\" style=\"width: 19%\">";
+      echo $post["price"];
+      echo "</td>";
+
       echo "</tr>";
     }
     echo "</table>";
